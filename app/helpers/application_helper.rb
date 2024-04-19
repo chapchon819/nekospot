69f2eb99dcce3aea38f04f2213715672e9c183f7
chapchon819 <l000merci.819@gmail.com>
2024-04-20 01:02:37 +0900

    ✨ Add:フラッシュメッセージ

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
-end
+    def flash_background_color(type)
+      case type.to_sym
+      when :notice then "p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400"
+      when :alert  then "p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+      when :success  then "p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
+      else "p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+      end
+    end
+  end
