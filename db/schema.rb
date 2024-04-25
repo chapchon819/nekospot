aac84c19a61f136a8d9b338e03e00b8b54a49c81
chapchon819 <l000merci.819@gmail.com>
2024-04-26 00:21:14 +0900

    ✨ Add:Categoryテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_23_163238) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_25_151342) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
