b3a469e85091331bb1386d006bf3a7088a2ce12b
chapchon819 <l000merci.819@gmail.com>
2024-04-30 17:25:05 +0900

    🚧 Update:info windowをcardに変更

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -2,9 +2,13 @@ class SpotsController < ApplicationController
 
   def map
     @user = current_user
-    @spots = Spot.all
-    @spots_json = @spots.to_json(only: [:id, :name, :latitude, :longitude, :address, :rating])
+    @spots = Spot.includes(:spot_images).all.map do |spot|
+      spot.as_json(only: [:id, :name, :latitude, :longitude, :address, :rating]).merge(
+        image: spot.spot_images.first&.image,
+        category: spot.category.name
+      )
+    end
+    @spots_json = @spots.to_json
   end
-
 
 end
