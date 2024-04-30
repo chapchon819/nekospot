52db86aa12a43be3787e8888997ad9419da62e3c
chapchon819 <l000merci.819@gmail.com>
2024-04-30 15:04:19 +0900

    🚧 Add:DBに登録したspotを一覧表示

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -2,5 +2,9 @@ class SpotsController < ApplicationController
 
   def map
     @user = current_user
+    @spots = Spot.all
+    @spots_json = @spots.to_json(only: [:id, :name, :latitude, :longitude, :address, :rating])
   end
+
+
 end
