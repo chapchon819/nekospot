27400b80ac2dd6445eb11932a82bcfdb0603ba92
chapchon819 <l000merci.819@gmail.com>
2024-05-01 16:10:13 +0900

    🎨 Update:footerのレイアウト調整

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -11,4 +11,15 @@ class SpotsController < ApplicationController
     @spots_json = @spots.to_json
   end
 
+  def list
+    @user = current_user
+    @spots = Spot.includes(:spot_images).all.map do |spot|
+      spot.as_json(only: [:id, :name, :latitude, :longitude, :address, :rating]).merge(
+        image: spot.spot_images.first&.image,
+        category: spot.category.name
+      )
+    end
+    @spots_json = @spots.to_json
+  end
+
 end
