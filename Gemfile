d776dc39750b6336123f28433be3ca2015910a85
chapchon819 <l000merci.819@gmail.com>
2024-05-18 15:31:51 +0900

    ✨ Add:バリデーションエラーの日本語化対応

--- Gemfile
+++ Gemfile
@@ -74,6 +74,9 @@ gem 'active_hash'
 # 検索
 gem 'ransack'
 
+# i18n
+gem 'rails-i18n', '~> 7.0.0'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,8 @@ module App
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.i18n.default_locale = :ja
+    onfig.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     # Configuration for the application, engines, and railties goes here.
     #
