f9cc0bfa6573f7ff2228946fd55d07975a47bd9b
chapchon819 <l000merci.819@gmail.com>
2024-05-19 03:35:15 +0900

    🩹 Fix:マイページ/投稿一覧のユーザー画像をOFFに

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
-    @reviews = @user.reviews.includes(:spot).order(created_at: :desc)
+    @reviews = @user.reviews.includes(:spot).order(created_at: :desc).page(params[:page]).per(10)
   end
 end
