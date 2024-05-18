65de00ec1b5e2bf288e81684d7d320734dfc8a2e
chapchon819 <l000merci.819@gmail.com>
2024-05-18 16:35:17 +0900

    ✨ Update:バリデーションエラーメッセージ調整

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :spot
 
-  validates :rating, presence: true
+  validates :rating, presence: true, numericality: { in: 1..5, message: "を入力してください" }
   validates :body, length: { maximum: 400 }
 end
--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module App
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
     config.i18n.default_locale = :ja
-    onfig.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     # Configuration for the application, engines, and railties goes here.
     #
