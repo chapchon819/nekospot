75e6c49dd7c46bdf37ac1dcaa77f3ac39025739e
chapchon819 <l000merci.819@gmail.com>
2024-06-07 14:18:39 +0900

    ✨ Update:画像が複数枚ある場合はカルーセルで全ての画像を見れるように修正

--- app/javascript/controllers/image_modal_controller.js
+++ app/javascript/controllers/image_modal_controller.js
@@ -2,19 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="image-modal"
 export default class extends Controller {
-  static targets = ["imageModal", "image"]
+  static targets = ["imageModal", "image", "carousel", "carouselImage"]
 
   connect() {
     this.hideModal();
+    this.currentIndex = 0;
     console.log("image-modal connected");
   }
 
   open(event) {
     event.preventDefault();
     console.log("image-modal open called");
-    const imageUrl = event.currentTarget.dataset.imageUrl;
-    this.imageTarget.src = imageUrl;
-    this.showModal();
+
+    if (this.hasCarouselTarget) {
+      this.showModal();
+      this.showImage(0);
+    } else {
+      const imageUrl = event.currentTarget.dataset.imageUrl;
+      this.imageTarget.src = imageUrl;
+      this.showModal();
+    }
   }
 
   close(event) {
@@ -28,5 +35,28 @@ export default class extends Controller {
 
   hideModal() {
     this.imageModalTarget.classList.add("hidden");
+  }
+
+  next() {
+    this.showImage(this.currentIndex + 1);
+  }
+
+  prev() {
+    this.showImage(this.currentIndex - 1);
+  }
+
+  showImage(index) {
+    if (index < 0) {
+      index = this.carouselImageTargets.length - 1;
+    } else if (index >= this.carouselImageTargets.length) {
+      index = 0;
+    }
+
+    this.carouselImageTargets.forEach((image, i) => {
+      image.classList.toggle("hidden", i !== index);
+      image.classList.toggle("block", i === index);
+    });
+
+    this.currentIndex = index;
   }
 }
