188ff8cf986d9c803577d9530d63f7faab98b852
chapchon819 <l000merci.819@gmail.com>
2024-06-08 22:40:30 +0900

    ⚡️ Update:Mapの検索パネルをスマホではサイドバーで表示されるように修正

--- app/javascript/controllers/search_accordion_controller.js
+++ app/javascript/controllers/search_accordion_controller.js
@@ -1,22 +1,51 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["panel"];
+  static targets = ["panel", "sidebar"];
 
   connect() {
-    this.panelTarget.style.maxHeight = "0px"; // 初期状態を閉じた状態に
+    this.closePanel(); // 初期状態でパネルを閉じる
   }
 
   toggle() {
-    const panel = this.panelTarget;
-    if (panel.style.maxHeight && panel.style.maxHeight !== "0px") {
-      panel.style.maxHeight = "0px";
-    } else {
-      panel.style.maxHeight = `${panel.scrollHeight}px`;
+    if (window.innerWidth >= 768) { // PCの場合
+      this.toggleAccordion();
+    } else { // スマホの場合
+      this.openSidebar();
     }
   }
 
+  toggleAccordion() {
+    const panel = this.panelTarget;
+    if (panel.classList.contains('h-0')) {
+      panel.classList.remove('h-0', 'opacity-0', 'invisible');
+      panel.classList.add('h-auto', 'opacity-100', 'visible');
+    } else {
+      this.closePanel();
+    }
+  }
+
+  openPanel() {
+    const panel = this.panelTarget;
+    panel.classList.remove('h-0', 'opacity-0', 'invisible');
+    panel.classList.add('h-auto', 'opacity-100', 'visible');
+  }
+
+  closePanel() {
+    const panel = this.panelTarget;
+    panel.classList.remove('h-auto', 'opacity-100', 'visible');
+    panel.classList.add('h-0', 'opacity-0', 'invisible');
+  }
+
+  openSidebar() {
+    this.sidebarTarget.style.display = "flex";
+  }
+
+  closeSidebar() {
+    this.sidebarTarget.style.display = "none";
+  }
+
   close() {
-    this.panelTarget.style.maxHeight = "0px";
+    this.closePanel();
   }
 }
