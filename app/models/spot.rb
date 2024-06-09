43d9b29bafa51624e48a8cb31175a695a107fce6
chapchon819 <l000merci.819@gmail.com>
2024-06-09 18:16:45 +0900

    ✨ Add:prioritized_spot_imageを作成しビューで猫画像を優先的に表示するよう修正

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -12,8 +12,13 @@ class Spot < ApplicationRecord
   belongs_to :prefecture, class_name: 'Prefecture', foreign_key: 'prefecture_id', optional: true
   validates :name, :latitude, :longitude, :place_id, presence: true, uniqueness: true
 
+  # 猫の画像を優先的に表示させるメソッド
+  def prioritized_spot_image #SpotImageの中で、cat属性がtrueのものを優先的に取得し、存在しない場合は最初のSpotImageを取得する
+    spot_images.find_by(cat: true) || spot_images.first
+  end
+
   # Geocoding setup
-  geocoded_by :address  # 使用する地名解決にはaddress属性を利用（ここはモデルの実際の属性に依存します）
+  geocoded_by :address  # 使用する地名解決にはaddress属性を利用（ここはモデルの実際の属性に依存）
   after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
 
   def self.ransackable_attributes(auth_object = nil)
