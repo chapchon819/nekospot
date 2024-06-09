88f492744dcc6a91ca1610498fed06721a735f28
chapchon819 <l000merci.819@gmail.com>
2024-06-09 16:07:02 +0900

    ✨ Add:Spot情報を保存する際に写真での猫の有無の判定結果を保存する機能を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_08_064932) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_09_063914) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_08_064932) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "cat", default: false
     t.index ["spot_id"], name: "index_spot_images_on_spot_id"
   end
 
