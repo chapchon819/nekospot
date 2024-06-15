4118e17a0d73caa052ae7f460ac9df29e77087fd
chapchon819 <l000merci.819@gmail.com>
2024-06-15 14:52:23 +0900

    ⚗️ Test:X埋め込み実験

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,4 @@
-Rails.application.config.session_store :active_record_store, :key => '_my_app_session', expire_after: 1.week
+Rails.application.config.session_store :active_record_store,
+    :key => '_my_app_session',
+    same_site: :lax, 
+    expire_after: 1.week
