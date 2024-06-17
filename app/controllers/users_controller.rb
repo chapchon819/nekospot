b15bcb311251f8988d9c3eaf5a18905d71b177c4
chapchon819 <l000merci.819@gmail.com>
2024-06-17 22:50:20 +0900

    ✨ Add:訪問済数などのリッチな表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @reviews = @user.reviews.includes(:spot).order(created_at: :desc).page(params[:page]).per(10)
+    @total_helpfuls = @user.reviews.joins(:helpfuls).count
   end
 end
