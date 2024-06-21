1de0dc726f2b2f28f3b4070964cec9c7ad1d0e29
chapchon819 <l000merci.819@gmail.com>
2024-06-22 02:35:51 +0900

    ✨ Update:セッションをRedisに保存

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,8 @@
-Rails.application.config.session_store :active_record_store,
-    :key => '_my_app_session',
-    same_site: :lax, 
-    expire_after: 1.week
+Rails.application.config.session_store :redis_store,
+    servers: %w(redis://nekospot-redis-1:6379/0/session),
+    # ホスト: docker環境のredisコンテナ名
+    # ポート: 6379
+    # DB: 0番
+    # ネームスペース: session
+    expire_after: 90.minutes, # 有効期限90分
+    key: "_#{Rails.application.class.module_parent_name.downcase}__session" # キー名
