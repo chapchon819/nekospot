bcc34728611c0408c050aae05189ca9fa39e5420
chapchon819 <l000merci.819@gmail.com>
2024-06-22 16:59:34 +0900

    ✨ Add:本番環境のredis設定

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,16 @@
-Rails.application.config.session_store :redis_store,
+if Rails.env.production?
+    Rails.application.config.session_store :redis_store,
+    servers: [
+        {
+        url: ENV['REDIS_URL'],
+        namespace: 'session'
+        }
+    ],
+    expire_after: 90.minutes,
+    key: "_#{Rails.application.class.module_parent_name.downcase}_session"
+else
+    Rails.application.config.session_store :redis_store,
     servers: %w(redis://nekospot-redis-1:6379/0/session),
-    # ホスト: docker環境のredisコンテナ名
-    # ポート: 6379
-    # DB: 0番
-    # ネームスペース: session
-    expire_after: 90.minutes, # 有効期限90分
-    key: "_#{Rails.application.class.module_parent_name.downcase}__session" # キー名
+    expire_after: 90.minutes,
+    key: "_#{Rails.application.class.module_parent_name.downcase}_session"
+end
