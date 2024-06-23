1ad8ff8d32fb6f1a8c8ebc05737bfc5d057fbb00
chapchon819 <l000merci.819@gmail.com>
2024-06-24 01:19:29 +0900

    ⚡️ Update:アセットパイプラインの画像の拡張子をwebpへ変更

--- app/helpers/diagnostics_helper.rb
+++ app/helpers/diagnostics_helper.rb
@@ -2,27 +2,27 @@ module DiagnosticsHelper
     def result_contents(category)
             case category.name
             when '猫カフェ'
-                { content: "猫スポットの代表！", image: 'cafe.png' } 
+                { content: "猫スポットの代表！", image: 'cafe.webp' } 
             when '島'
-                { content: "人間より猫の方が多い?!", image: 'island.png' } 
+                { content: "人間より猫の方が多い?!", image: 'island.webp' } 
             when '動物園'
-                { content: "猫にも会える！", image: 'zoo.png' } 
+                { content: "猫にも会える！", image: 'zoo.webp' } 
             when '宿泊施設'
-                { content: "看板猫がいる", image: 'hotel.png' }
+                { content: "看板猫がいる", image: 'hotel.webp' }
             when '駅・バス停'
-                { content: "看板猫がいる", image: 'default.png' }
+                { content: "看板猫がいる", image: 'default.webp' }
             when '飲食店'
-                { content: "看板猫がいる", image: 'eating.png' }
+                { content: "看板猫がいる", image: 'eating.webp' }
             when 'アニマルカフェ'
-                { content: "猫以外の動物ともふれあえる！", image: 'animal_cafe.png' }
+                { content: "猫以外の動物ともふれあえる！", image: 'animal_cafe.webp' }
             when '書店・雑貨店'
-                { content: "看板猫がいる", image: 'shop.png' }
+                { content: "看板猫がいる", image: 'shop.webp' }
             when '公園'
-                { content: "猫とのふれあいスペースがある！", image: 'park.png' } 
+                { content: "猫とのふれあいスペースがある！", image: 'park.webp' } 
             when 'シェルター'
-                { content: "新しい家族に出会えるかも？", image: 'shelter.png' }
+                { content: "新しい家族に出会えるかも？", image: 'shelter.webp' }
             when '銭湯'
-                { content: "看板猫がいる", image: 'furo.png' }
+                { content: "看板猫がいる", image: 'furo.webp' }
             else 
                 "おすすめの猫スポットはありませんでした"
         end
