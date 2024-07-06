9d9c63cf66f05a3058cd41f17d0630788c614f22
chapchon819 <l000merci.819@gmail.com>
2024-07-07 00:37:34 +0900

    🔐 Add:sidekiq管理画面にBasic認証設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  Sidekiq::Web.use(Rack::Auth::Basic) do |user_id, password|
+    [user_id, password] == [ENV['SIDEKIQ_BASIC_ID'], ENV['SIDEKIQ_BASIC_PASSWORD']]
+  end
+  mount Sidekiq::Web, at: '/sidekiq'
   get 'rewiews/new'
   get 'rewiews/create'
   get 'users/show'
