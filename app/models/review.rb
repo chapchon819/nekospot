19b159978c38c81b9288ba696c6e33d13005adf6
chapchon819 <l000merci.819@gmail.com>
2024-07-17 02:55:07 +0900

    ✨ Add:タグのバリデーション追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,7 +4,7 @@ class Review < ApplicationRecord
   has_many :helpfuls, dependent: :destroy
   has_many :review_tags, dependent: :destroy
   has_many :tags, through: :review_tags
-  attr_accessor :images_cache
+  attr_accessor :images_cache, :tag_names
   mount_uploaders :images, ImageUploader
   process_in_background :images
 
@@ -12,6 +12,7 @@ class Review < ApplicationRecord
   validates :rating, presence: true, numericality: { in: 1..5, message: "を入力してください" }
   validates :body, length: { maximum: 400 }
   validate :validate_image_count
+  validate :validate_tag_count
   #validate :validate_images
 
   # 画像削除を処理するメソッド
@@ -29,14 +30,17 @@ class Review < ApplicationRecord
     ["spot", "user"]
   end
 
-  def save_tags(save_review_tags)
-  # 既存のタグをクリアすることで、タグ編集時の重複を防ぐ
-  self.tags.clear
+  def save_tags(tag_list)
+    if tag_list.size > 5
+      errors.add(:tags, "タグは5件までです。")
+      return false
+    end
 
-  save_review_tags.each do |new_name| #カラムの中から同じ値がないか探して、あればそのままfindの動き、なければcreateの動きで新たにカラムに保存
-    review_tag = Tag.find_or_create_by(name: new_name)
-    self.tags << review_tag #タグを関連付ける
-  end
+    self.tags.clear
+    tag_list.each do |new_name|
+      review_tag = Tag.find_or_create_by(name: new_name)
+      self.tags << review_tag
+    end
   end
 
   # レビューの文字数を判定
@@ -52,6 +56,12 @@ class Review < ApplicationRecord
   def validate_image_count
     if images.size > 3
       errors.add(:images, "画像のアップロードは３枚までです。")
+    end
+  end
+
+  def validate_tag_count
+    if tags.size > 5
+      errors.add(:tags, "タグは5件までです。")
     end
   end
 
