f74455913c73de3c5720e38b89c8fb2010718fca
chapchon819 <l000merci.819@gmail.com>
2024-07-20 13:36:25 +0900

    🐛 Fix:ページネーションのレイアウト崩れ修正

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -3,7 +3,7 @@
 Kaminari.configure do |config|
   config.default_per_page = 25
   config.window = 1    # 現在のページの前後に表示するページリンクの数を指定します
-  config.outer_window = 1    # 最初と最後のページリンクの数を指定します
+  config.outer_window = 0    # 最初と最後のページリンクの数を指定します
   config.left = 0      # 現在のページの左側に表示するページリンクの数を指定します
   config.right = 0     # 現在のページの右側に表示するページリンクの数を指定します
 end
