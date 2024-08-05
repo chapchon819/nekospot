d2f2e4b0a4c66d0e8d97c75d776ddd291cec7aa6
chapchon819 <l000merci.819@gmail.com>
2024-08-06 01:34:58 +0900

    🩹 Fix:軽微な修正

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,41 +2,41 @@ require 'rails_helper'
 
 RSpec.describe Review, type: :model do
   describe 'validations' do
-    it 'is valid with valid attributes' do
+    it '1) 有効な属性を持つレビューが有効であること' do
       review = build(:review)
       expect(review).to be_valid
     end
 
-    it 'ratingがない場合にレビューを無効にする' do
+    it '2) ratingがない場合にレビューを無効にする' do
       review = build(:review, rating: nil)
       expect(review).not_to be_valid
       expect(review.errors[:rating]).to include("を入力してください")
     end
 
-    it 'ratingが1未満の場合にレビューを無効にする' do
+    it '3) ratingが1未満の場合にレビューを無効にする' do
       review = build(:review, rating: 0)
       expect(review).not_to be_valid
       expect(review.errors[:rating]).to include("を入力してください")
     end
 
-    it 'ratingが6以上の場合にレビューを無効にする' do
+    it '4) ratingが6以上の場合にレビューを無効にする' do
       review = build(:review, rating: 6)
       expect(review).not_to be_valid
       expect(review.errors[:rating]).to include("を入力してください")
     end
 
-    it 'is valid without a body' do
+    it '5) bodyなしでも有効であること' do
       review = build(:review, body: nil)
       expect(review).to be_valid
     end
 
-    it 'bodyの文字数が400以上の場合にレビューを無効にする' do
+    it '6) bodyの文字数が400以上の場合にレビューを無効にする' do
       review = build(:review, body: 'a' * 401)
       expect(review).not_to be_valid
       expect(review.errors[:body]).to include("は400文字以内で入力してください")
     end
 
-    it '画像が3枚以上の場合にレビューを無効にする' do
+    it '7) 画像が3枚以上の場合にレビューを無効にする' do
       review = build(:review)
       review.images = [
         Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/sample1.jpg'), 'image/jpg'),
@@ -48,7 +48,7 @@ RSpec.describe Review, type: :model do
       expect(review.errors[:images]).to include("画像のアップロードは３枚までです。")
     end
 
-    it 'tagが6個以上の場合にレビューを無効にする' do
+    it '8) tagが6個以上の場合にレビューを無効にする' do
       review = build(:review)
       review.tags << create_list(:tag, 6)
       expect(review).not_to be_valid
